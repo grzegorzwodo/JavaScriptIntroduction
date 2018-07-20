30ab6ebbc6e612b3e5536d0cccee807a6dc9d339
Grzegorz <grzegorz.wodo@gmail.com>
2018-07-20 20:13:15 +0200

    .

--- web/js/jquery.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-    $(".rectangle-menu").hover(function() {
-        $(this).animate({left: "0px"}, 500);
-    }, function() {
-        $(this).animate({left: "-300px"}, 500);
-    });
-});
