06f9b1f912c3f02c58800399d18ba2e3f804fd94
Grzegorz <grzegorz.wodo@gmail.com>
2018-07-21 12:15:49 +0200

    JS

---
+++ web/js/jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $(".rectangle-menu").hover(function(){
+        $(this).stop();
+        $(this).animate({left: '0px'});
+    }, function(){
+        $(this).stop();
+        $(this).animate({left: '-280px'});
+    });
+
+});
---
+++ web/js/jquery3.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $(".submenu1").click(function() {
+        $("#submenu1").slideToggle();
+    });
+    $(".submenu2").click(function() {
+       $("#submenu2").slideToggle();
+    });
+    $(".submenu3").click(function() {
+       $("#submenu3").slideToggle();
+    });
+});
