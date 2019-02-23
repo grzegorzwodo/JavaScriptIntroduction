0a69846caa4d4095e770fc6c313d38a8a2b6b6f2
Grzegorz <grzegorz.wodo@gmail.com>
2019-02-23 10:17:17 +0100

    .

--- web/js/script.js
+++ web/js/script.js
@@ -1,6 +1,11 @@
 function changeColor(color) {
      console.log(color);
+     let body = document.getElementsByTagName("body");
+     //body[0].style="background-color: "+color;
  }
 
  function changeColorOnClick() {
+    let select = document.getElementById("color");
+    let body = document.getElementsByTagName("body");
+    body[0].style="background-color: " + select.value;
  }
