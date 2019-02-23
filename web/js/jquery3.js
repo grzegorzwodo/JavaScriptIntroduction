9a9c5642d8426f02b7fa85ed135bcc52661bfb91
Grzegorz <grzegorz.wodo@gmail.com>
2019-02-23 12:28:09 +0100

    rozwiazania

--- web/js/jquery3.js
+++ web/js/jquery3.js
@@ -3,7 +3,14 @@ $(document).ready(function(){
    //możemy wykonywać na nim operacje JS
    //$(selector).action()
    $(".submenu1 span").click(function() {
-        console.log("Test");
+        $("#submenu1").toggle(600);
    });
 
+   $(".submenu2 span").click(function() {
+        $("#submenu2").toggle(600);
+   });
+
+   $(".submenu3 span").click(function() {
+        $("#submenu3").toggle(600);
+   });
 })
--- web/js/script.js
+++ web/js/script.js
@@ -1,11 +1,50 @@
 function changeColor(color) {
-     console.log(color);
-     let body = document.getElementsByTagName("body");
-     //body[0].style="background-color: "+color;
- }
+       console.log(color);
+       let body = document.getElementsByTagName("body");
+       //body[0].style="background-color: "+color;
+   }
 
- function changeColorOnClick() {
-    let select = document.getElementById("color");
-    let body = document.getElementsByTagName("body");
-    body[0].style="background-color: " + select.value;
- }
+   function changeColorOnClick() {
+      let select = document.getElementById("color");
+      let body = document.getElementsByTagName("body");
+      body[0].style="background-color: " + select.value;
+   }
+
+
+function validateNameOrSurname(obj) {
+    if(obj.value.length >= 2) {
+        obj.classList.remove("error");
+    }else {
+        obj.classList.add("error");
+    }
+}
+
+function validateStreet(obj) {
+    if(obj.value.length >= 1) {
+        obj.classList.remove("error");
+    }else {
+        obj.classList.add("error");
+    }
+}
+
+function validatePostalCode(obj) {
+    var rePostalCode = /(^[0-9]{2}\-[0-9]{3}$)/i;
+    if(rePostalCode.test(obj.value)) {
+        console.log("remove error");
+        obj.classList.remove("error");
+    }else {
+        console.log("add error");
+        obj.classList.add("error");
+    }
+}
+
+function validateEmailAddress(obj) {
+    console.log(obj.value);
+    if(validateEmail(obj.value)) {
+        console.log("remove error");
+        obj.classList.remove("error");
+    }else {
+        console.log("add error");
+        obj.classList.add("error");
+    }
+}
