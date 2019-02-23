8fdccae87c7281152004d7e0f5a01c8f9962d96b
Grzegorz <grzegorz.wodo@gmail.com>
2019-02-23 12:27:31 +0100

    rozwiazania

---
+++ web/js/jquery3.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   //dokument html załadowany do przeglądarki
+   //możemy wykonywać na nim operacje JS
+   //$(selector).action()
+   $(".submenu1 span").click(function() {
+        console.log("Test");
+   });
+
+})
